d5550abc8e9d2d180060c13ce05200621c86348b
AgyenJu <Justiceagyen@gmail.com>
2024-07-30 20:48:08 +0000

    javacsript

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,26 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
+const path = require('path');
+
+// Get the file path from the command-line arguments
+const filePath = process.argv[2];
+
+if (!filePath) {
+    console.error('Please provide a file path as the first argument.');
+    process.exit(1);
+}
+
+// Resolve the full path
+const fullPath = path.resolve(filePath);
+
+// Read the file in UTF-8 encoding
+fs.readFile(fullPath, 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading the file:', err);
+        return;
+    }
+
+    // Print the file content
+    console.log(data);
 });
